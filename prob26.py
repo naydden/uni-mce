b351549e2e4b688a22922d40ae5eb98b2a643f2a
Boyan Naydenov <boyan.naydenov@gmail.com>
2017-05-18 13:09:21 +0200

    Small refactor

--- prob26.py
+++ prob26.py
@@ -8,11 +8,10 @@ result = 0;
 sizes = [];
 d = range(1,1000);
 for i in d:
-	status = False;
 	previous_reminders = [];
 	reminder = 1;
 	size = 0;
-	while(not status):
+	while True:
 		size += 1;
 		previous_reminders.extend([reminder]);
 		if size == 1:
@@ -22,7 +21,7 @@ for i in d:
 			# if reminder already existed, then we hit the end of the cycle
 			#  also if size reaches i-1 level, we can stop since there will be no more
 			if reminder in previous_reminders or size == (i-1):
-				status = True;
+				break;
 	sizes.extend([size]);
 
 print d[max(sizes)]
